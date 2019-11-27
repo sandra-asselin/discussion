6b2f098054fb512f88d05faa8e6e86f14f40ead8
Clément Caillat <clement-caillat@laplateforme.io>
2019-11-27 13:34:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Mon site</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<header>
+		</header>
+		<main>
+		</main>
+		<footer>
+		</footer>
+	</body>
+</html>
